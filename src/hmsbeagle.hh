63a23204aadbf768400cf3c1ef95e1a7cad293ce
Erick Matsen <matsen@fhcrc.org>
2012-10-13 14:02:12 -0700

    a bit of sorting

--- src/hmsbeagle.hh
+++ src/hmsbeagle.hh
@@ -18,6 +18,8 @@
 
 std::vector< double > get_partials(const std::string& sequence);
 
+// XXX should we come up with a convention for method order? I'd be happy with either approximate dependency, split into
+// public and private or not.
 class OnlineCalculator
 {
 public:
@@ -26,26 +28,26 @@ public:
         beagleFinalizeInstance(instance);
     };
 
-    double calculate_ll(std::shared_ptr< phylo_node > node, std::vector<bool>& visited);
     void initialize(const std::vector<std::string>& seqs);
     int get_id();
     void free_id(int id);
+    double calculate_ll(std::shared_ptr< phylo_node > node, std::vector<bool>& visited);
+
 private:
     BeagleInstanceDetails instDetails;
     int stateCount;
     int nPatterns;
     int nPartBuffs;
     int instance;
-
     int next_id;
     std::stack<int> free_ids;
     // AD: Is this a naming convention of src_dst for maps? Was surprising for me. Could I change to map_id_ll?
     std::unordered_map< int, double > id_ll; // caches the root ll at each ID
 
     int create_beagle_instance();
+    void grow();
     void set_eigen_and_rates_and_weights(int instance);
     void set_eigen_and_rates_and_weights(int instance, const bpp::AbstractReversibleSubstitutionModel& model);
-    void grow();
 };
 
 /// Get the ID of an avaiable partial buffer.
