ec255502057c71745daf3f766cd53c3bba662395
Connor McCoy <cmccoy@fhcrc.org>
2012-10-15 09:05:21 -0700

    Implementation of bpp-style get_partials

--- src/bpp_shim.cc
+++ src/bpp_shim.cc
@@ -1,5 +1,6 @@
-
 #include "bpp_shim.hh"
+#include <string>
+
 
 void
 blit_vector_to_array(double *arr, const std::vector<double> &vec)
@@ -18,6 +19,23 @@ blit_matrix_to_array(double *arr, const bpp::Matrix<double> &matrix)
     }
 }
 
+/// Get a vector of partial states from a sequence, substitution model, and alphabet.
+/// This does not have site compression, which would require buying into Bio++'s sitepatterns object.
+std::vector<double> get_partials(const std::string& sequence, const bpp::SubstitutionModel &model, const bpp::Alphabet *alphabet) {
+    unsigned int n_states = model.getNumberOfStates(), n_sites = sequence.length();
+    const std::string title = std::string();
+
+    std::vector<double> partials(n_sites * n_states);
+
+    const bpp::BasicSequence seq = bpp::BasicSequence(title, sequence, alphabet);
+    for (unsigned int site = 0; site < n_sites; site++) {
+        for (unsigned int i = 0; i < n_states; i++) {
+            partials[n_states*site + i] = model.getInitValue(i, seq.getValue(site));
+        }
+    }
+
+    return partials;
+}
 
 /*
 
--- src/bpp_shim.hh
+++ src/bpp_shim.hh
@@ -6,8 +6,12 @@
 #define __bpp_shim__
 
 #include <Bpp/Numeric/Matrix/Matrix.h>
+#include <Bpp/Phyl/Model/SubstitutionModel.h>
+#include <Bpp/Seq/Alphabet/Alphabet.h>
+#include <Bpp/Seq/Sequence.h>
 
 void blit_vector_to_array(double *, const std::vector<double> &);
 void blit_matrix_to_array(double *, const bpp::Matrix<double> &);
+std::vector<double> get_partials(const std::string&, const bpp::SubstitutionModel &, const bpp::Alphabet *);
 
 #endif //  __bpp_shim__
