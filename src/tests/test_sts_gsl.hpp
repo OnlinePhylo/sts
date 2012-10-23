f43e3f787cd6925dece993ca638e5c0d88061984
Connor McCoy <cmccoy@fhcrc.org>
2012-10-23 14:36:50 -0700

    Add GSL minimization
    & factor out tests a bit

---
+++ src/tests/test_sts_gsl.hpp
@@ -0,0 +1,29 @@
+#ifndef TEST_STS_GSL_HPP
+#define TEST_STS_GSL_HPP
+
+#include <cmath>
+#include "catch.hpp"
+#include "sts/gsl.hpp"
+
+namespace sts
+{
+namespace test
+{
+namespace gsl
+{
+
+struct SquareFn
+{
+    double operator()(double d) { return d * d; }
+};
+
+TEST_CASE( "sts/gsl/minimize", "sts::gsl::minimize is a wrapper for GSL minimization functions" ) {
+    SquareFn instance;
+    REQUIRE(std::abs(sts::gsl::minimize(instance, -100.0, -1000, 1000.0)) < 1e-5);
+}
+
+} //gsl
+} //test
+} //sts
+
+#endif //TEST_STS_GSL_HPP
