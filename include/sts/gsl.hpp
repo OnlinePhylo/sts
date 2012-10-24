aa33809cf26b5ca8d9a7527179a90b3d9f346cca
Connor McCoy <cmccoy@fhcrc.org>
2012-10-24 12:01:32 -0700

    GSL updates

--- include/sts/gsl.hpp
+++ include/sts/gsl.hpp
@@ -16,19 +16,23 @@ namespace gsl
 /// \param data a std::function<double(double)>
 double std_func_to_gsl_function(double d, void *data)
 {
-    std::function<double(double)> *func = (std::function<double(double)>*)(data);
+    std::function<double(double)> *func = reinterpret_cast<std::function<double(double)>*>(data);
     return (*func)(d);
 }
 
-double minimize(const std::function<double(double)> fn, double m=0.5, double a=0, double b=1, const int max_iter=100,
-                const gsl_min_fminimizer_type *min_type=gsl_min_fminimizer_brent)
+double minimize(const std::function<double(double)> fn,
+        double m=0.5,
+        double a=0,
+        double b=1,
+        const int max_iter=100,
+        const gsl_min_fminimizer_type *min_type=gsl_min_fminimizer_brent)
 {
     int iter = 0, status;
     gsl_min_fminimizer *s;
     gsl_function gsl_fn;
 
     gsl_fn.function = &std_func_to_gsl_function;
-    gsl_fn.params = (void*)&fn;
+    gsl_fn.params = reinterpret_cast<void*>(&fn);
 
     s = gsl_min_fminimizer_alloc (min_type);
     gsl_min_fminimizer_set(s, &gsl_fn, m, a, b);
