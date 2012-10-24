29d3c51d46f42dbb013764bc530e836ae65756d0
Connor McCoy <cmccoy@fhcrc.org>
2012-10-23 17:04:37 -0700

    Failing likelihood tests
    Need to fix tree reading.

--- src/tests/test_sts_likelihood.hpp
+++ src/tests/test_sts_likelihood.hpp
@@ -3,8 +3,16 @@
 #ifndef STS_TEST_LIKELIHOOD_HPP
 #define STS_TEST_LIKELIHOOD_HPP
 
+#include <fstream>
+#include <memory>
+#include <streambuf>
 #include <string>
+#include <vector>
+
+#include <Bpp/Seq/Alphabet/DNA.h>
+#include <Bpp/Phyl/Model/JCnuc.h>
 #include "sts/likelihood/online_calculator.hpp"
+#include "sts/util.hpp"
 #include "catch.hpp"
 
 namespace sts
@@ -13,9 +21,31 @@ namespace test
 {
 namespace parsing
 {
+    std::string slurp(const std::string file_name)
+    {
+        std::ifstream s(file_name);
+        std::string str((std::istreambuf_iterator<char>(s)),
+                         std::istreambuf_iterator<char>());
+        return str;
+    }
+
     TEST_CASE("sts/likelihood/known_tree", "Test calculating the likelihood of a known tree")
     {
-        int i = 0;
+        const bpp::DNA dna;
+        std::ifstream aln_stream("../data/bppsim/JC69/JC69.fasta");
+        //std::ifstream nw_stream("../data/bppsim/JC69/JC69.dnd");
+        std::string nwk_string = slurp("../data/bppsim/JC69/JC69.dnd");
+        auto aln = std::shared_ptr<bpp::SiteContainer>(sts::util::read_alignment(aln_stream, &dna));
+        REQUIRE(aln->getNumberOfSequences() == 79);
+
+        auto model = std::shared_ptr<bpp::SubstitutionModel>(new bpp::JCnuc(&dna));
+        auto calc = std::make_shared<sts::likelihood::online_calculator>();
+        calc->initialize(aln, model);
+        auto root = sts::util::phylo_of_newick_string(calc, nwk_string);
+        std::vector<bool> visited;
+        double ll = calc->calculate_ll(root->node, visited);
+        REQUIRE(1 == ll); // FALSE?
+        REQUIRE(false);
     }
 }
 }
