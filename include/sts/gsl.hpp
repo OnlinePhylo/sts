0a03b9c0721ffc6c1ce75afb316a0dd5ef98da32
Connor McCoy <cmccoy@fhcrc.org>
2012-10-24 12:03:12 -0700

    make style

--- include/sts/gsl.hpp
+++ include/sts/gsl.hpp
@@ -21,11 +21,11 @@ double std_func_to_gsl_function(double d, void *data)
 }
 
 double minimize(const std::function<double(double)> fn,
-        double m=0.5,
-        double a=0,
-        double b=1,
-        const int max_iter=100,
-        const gsl_min_fminimizer_type *min_type=gsl_min_fminimizer_brent)
+                double m = 0.5,
+                double a = 0,
+                double b = 1,
+                const int max_iter = 100,
+                const gsl_min_fminimizer_type *min_type = gsl_min_fminimizer_brent)
 {
     int iter = 0, status;
     gsl_min_fminimizer *s;
@@ -34,11 +34,10 @@ double minimize(const std::function<double(double)> fn,
     gsl_fn.function = &std_func_to_gsl_function;
     gsl_fn.params = reinterpret_cast<void*>(&fn);
 
-    s = gsl_min_fminimizer_alloc (min_type);
+    s = gsl_min_fminimizer_alloc(min_type);
     gsl_min_fminimizer_set(s, &gsl_fn, m, a, b);
 
-    do
-    {
+    do {
         iter++;
         status = gsl_min_fminimizer_iterate(s);
 
@@ -46,9 +45,8 @@ double minimize(const std::function<double(double)> fn,
         a = gsl_min_fminimizer_x_lower(s);
         b = gsl_min_fminimizer_x_upper(s);
 
-        status = gsl_min_test_interval (a, b, 0.001, 0.0);
-    }
-    while (status == GSL_CONTINUE && iter < max_iter);
+        status = gsl_min_test_interval(a, b, 0.001, 0.0);
+    } while(status == GSL_CONTINUE && iter < max_iter);
     gsl_min_fminimizer_free(s);
     return m;
 }
--- src/tests/test_sts_gsl.hpp
+++ src/tests/test_sts_gsl.hpp
@@ -12,12 +12,12 @@ namespace test
 namespace gsl
 {
 
-struct SquareFn
-{
+struct SquareFn {
     double operator()(double d) { return d * d; }
 };
 
-TEST_CASE( "sts/gsl/minimize", "sts::gsl::minimize is a wrapper for GSL minimization functions" ) {
+TEST_CASE("sts/gsl/minimize", "sts::gsl::minimize is a wrapper for GSL minimization functions")
+{
     SquareFn instance;
     REQUIRE(std::abs(sts::gsl::minimize(instance, -100.0, -1000, 1000.0)) < 1e-5);
 }
