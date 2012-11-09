8a7dd7d181efa2541f1fa01c0444997ee45335e3
Connor McCoy <cmccoy@fhcrc.org>
2012-11-09 13:29:58 -0800

    Remove aggregate includes

--- include/sts/likelihood.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef STS_LIKELIHOOD_HPP
-#define STS_LIKELIHOOD_HPP
-
-#include "sts/likelihood/online_calculator.hpp"
-// Recursive dependency
-#include "sts/particle/node.hpp"
-#include "sts/likelihood/forest_likelihood.hpp"
-
-#endif // STS_LIKELIHOOD_HPP
