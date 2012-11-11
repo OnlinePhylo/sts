b382599a2a0177bc22392a0113546acd526fd3f2
Erick Matsen <matsen@fhcrc.org>
2012-11-11 12:39:56 -0800

    Issue #59 still persists with non-delta bl-dens

--- data/bppsim/cherry/run_sts.sh
+++ data/bppsim/cherry/run_sts.sh
@@ -1,2 +1,2 @@
-GSL_RNG_SEED=1 ../../../_build/sts seq.fasta -p 198 --bl-dens delta | sort -n -r | uniq > sts.sort.uniq.out
+GSL_RNG_SEED=1 ../../../_build/sts seq.fasta -p 199 | sort -n -r | uniq > sts.sort.uniq.out
 head -n 1 sts.sort.uniq.out | cut -f 2 > sts.tre
