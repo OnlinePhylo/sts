45e3232873357ad51170e366e1f8a22be499dbd3
Connor McCoy <cmccoy@fhcrc.org>
2012-11-13 16:52:14 -0800

    WIP - include branch length proposer

--- src/child_swap_mcmc_move.h
+++ src/child_swap_mcmc_move.h
@@ -4,6 +4,8 @@
 #ifndef STS_MOVES_CHILD_SWAP_MCMC_MOVE_H
 #define STS_MOVES_CHILD_SWAP_MCMC_MOVE_H
 
+#include "branch_length_proposer.h"
+
 #include "mcmc_move.h"
 
 #include "smctc.hh"
@@ -19,9 +21,13 @@ namespace moves
 class Child_swap_mcmc_move : public Mcmc_move
 {
 public:
-    Child_swap_mcmc_move(sts::likelihood::Forest_likelihood& log_likelihood) : Mcmc_move(log_likelihood) {};
+    Child_swap_mcmc_move(sts::likelihood::Forest_likelihood& log_likelihood, Branch_length_proposer *bl_proposer) :
+        Mcmc_move(log_likelihood),
+        branch_length_proposer(bl_proposer) {};
 
     void propose_move(long time, particle::Particle& part, smc::rng* rng) const;
+protected:
+    Branch_length_proposer branch_length_proposer;
 };
 
 } // namespace moves
