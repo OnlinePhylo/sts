b774566700549d419bfbd7cebdb5f73e04044bc1
Connor McCoy <cmccoy@fhcrc.org>
2013-01-10 10:17:20 -0800

    Fixes to suppress clang warnings

--- src/forest_likelihood.h
+++ src/forest_likelihood.h
@@ -22,18 +22,18 @@ public:
     ///  \param calc Initialized likelihood calculator
     ///  \param leaf_nodes Vector representing \\perp
     explicit Forest_likelihood(std::shared_ptr<Online_calculator> calc,
-                               std::vector<particle::Node_ptr> leaf_nodes) : calc(calc), leaf_nodes(leaf_nodes) {};
-    /// Copy constructor
-    explicit Forest_likelihood(const Forest_likelihood &other) : calc(other.calc), leaf_nodes(other.leaf_nodes) {};
+                               std::vector<sts::particle::Node_ptr> leaf_nodes) :
+        calc(calc),
+        leaf_nodes(leaf_nodes) {};
 
     double operator()(const particle::Particle&) const;
     double calculate_log_likelihood(const particle::Particle&) const;
 
-    const std::vector<particle::Node_ptr> get_leaves() const;
+    const std::vector<sts::particle::Node_ptr> get_leaves() const;
     std::shared_ptr<Online_calculator> get_calculator() const;
 private:
     std::shared_ptr<Online_calculator> calc;
-    std::vector<particle::Node_ptr> leaf_nodes;
+    std::vector<sts::particle::Node_ptr> leaf_nodes;
 };
 } // namespace likelihood
 } // namespace sts
