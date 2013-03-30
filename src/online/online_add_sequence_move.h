71c7f80fc7f14260980dc5202acb712acec16449
Connor McCoy <cmccoy@fhcrc.org>
2013-03-29 23:04:24 -0700

    Notes

--- src/online/online_add_sequence_move.h
+++ src/online/online_add_sequence_move.h
@@ -21,9 +21,24 @@ struct Branch_lengths
 
 /// \brief Adds a taxon to a tree.
 ///
+/// Adds a taxon, \f$s\f$ to a particle using a pplacer-style proposal density on edges:
+///
+/// - Two passes are made over the tree; likelihood vectors are cached for both sides of every edge
+/// - We calculate the log-likelihood of attaching \f$s\f$ at the midpoint of every edge (linear in number of edges, with cached vectors).
+/// - Mid-edge LnLs form proposal density on edges
+/// - Some ML-optimization is performed for attachment location on edge, pendant branch lengths; a branch length is
+///   proposed from a distribution around these lengths.
+///
 /// For each time in <c>[1,taxa_to_add.size()]</c>, adds <c>taxa_to_add[time-1]</c> to the tree.
-
+///
 /// **NOTE**: All of the sequences in taxa_to_add must have partials registered in calculator.
+///
+/// The particle weight is updated using:
+/// \f[
+/// w_{r,k} = \frac{\gamma*(s_{r,k})}{\gamma*(s_{r-1,k})q(s_{r-1,k}\rightarrow s_{r,k})}
+/// \f]
+/// Where \f$\gamma*\f$ is the log-likelihood of the tree with \f$n-1\f$ and \f$n\f$ taxa, and the proposal density
+/// \f$q(s_{r-1,k}\rightarrow s_{r,k})\f$ uses the mid-edge log-likelihood
 class Online_add_sequence_move
 {
 public:
@@ -42,7 +57,6 @@ public:
                                               const std::string& leaf_name,
                                               smc::rng* rng);
     int operator()(long, smc::particle<Tree_particle>&, smc::rng*);
-
 
     Branch_lengths propose_branch_lengths(const bpp::Node* insert_edge, const std::string& new_leaf_name);
 
