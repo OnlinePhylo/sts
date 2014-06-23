17b3aceb385914629620f62b245df3e6f50decc3
Connor McCoy <cmccoy@fhcrc.org>
2014-06-23 09:43:32 -0700

    Search standard paths for bpp

--- lib/cmake/FindBpp.cmake
+++ lib/cmake/FindBpp.cmake
@@ -3,12 +3,18 @@ if (BPP_INCLUDES)
   set (BPP_FIND_QUIETLY TRUE)
 endif (BPP_INCLUDES)
 
-find_path(BPP_CORE_INCLUDES "Bpp/Numeric.all")
-find_library(BPP_CORE_LIBRARIES bpp-core)
-find_path(BPP_SEQ_INCLUDES "Bpp/Seq.all")
-find_library(BPP_SEQ_LIBRARIES bpp-seq)
-find_path(BPP_PHYL_INCLUDES "Bpp/Phyl.all")
-find_library(BPP_PHYL_LIBRARIES bpp-phyl)
+find_path(BPP_CORE_INCLUDES "Bpp/Numeric.all"
+  PATHS ENV CPATH)
+find_library(BPP_CORE_LIBRARIES bpp-core
+  PATHS ENV LIBS ENV LIBRARY_PATH)
+find_path(BPP_SEQ_INCLUDES "Bpp/Seq.all"
+  PATHS ENV CPATH)
+find_library(BPP_SEQ_LIBRARIES bpp-seq
+  PATHS ENV LIBS ENV LIBRARY_PATH)
+find_path(BPP_PHYL_INCLUDES "Bpp/Phyl.all"
+  PATHS ENV CPATH)
+find_library(BPP_PHYL_LIBRARIES bpp-phyl
+  PATHS ENV LIBS ENV LIBRARY_PATH)
 set(BPP_LIBRARIES
   ${BPP_CORE_LIBRARIES}
   ${BPP_SEQ_LIBRARIES}
